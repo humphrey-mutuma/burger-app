a2d2702facc10174c8b4c1d5c7d6c8f71b36b176
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-20 09:09:46 +0300

    Fix errors

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-import classes from './BuildControl.css'
+import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
@@ -10,4 +10,4 @@ const buildControl = (props) => (
     </div>
 );
 
-export default buildControl
+export default buildControl;
