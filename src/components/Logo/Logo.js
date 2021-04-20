4ea61c030d513532b2d595354158d059394c033e
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-20 18:56:08 +0300

    adjust responiveness

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
-import classes from './logo.css';
+import classes from './Logo.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:props.height}}>
         <img src={burgerLogo} alt="My Burger"/>
     </div>
 )
