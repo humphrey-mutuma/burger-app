1e55d5bd915a7f650e607f1bcbd8e3aec4314e59
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-20 13:20:17 +0300

    Remove backdrop on click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,9 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing:true});
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing:false});
+    }
 
     render () { 
         const disabledInfo = {
@@ -85,7 +88,7 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: false, ...}
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
 
