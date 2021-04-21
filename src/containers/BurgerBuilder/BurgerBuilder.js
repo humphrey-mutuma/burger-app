7aaad87051f14c7db55f647bde1f48b3a295a427
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-21 20:31:38 +0300

    Handle errors with axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import withErrorHandler from '../../hoc/withErroeHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../../src/axios-orders';
 
@@ -142,4 +143,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder; 
+export default withErrorHandler(BurgerBuilder, axios) ; 
